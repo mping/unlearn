d32778670c359cc8ee65db5f40b1baaf7a4a1aa5
Miguel Ping <miguel.ping@gmail.com>
2020-10-09 23:04:11 +0100

    use faster version

--- src/unlearn/virtual/macros.clj
+++ src/unlearn/virtual/macros.clj
@@ -6,35 +6,33 @@
             [manifold.deferred :as d]
             [criterium.core :as cc])
   (:import (java.util.concurrent ExecutorService CompletableFuture)
-           (java.util.function Function Supplier)))
+           (java.util.function Function Supplier)
+           (java.lang AutoCloseable)))
 
 (defmacro task [& body]
   ;; todo submit to executor
   `(fn [] ~@body))
 
-
 (defn- vars-of [binding-pairs]
   (let [[vars sexprs] (apply map list binding-pairs)]
     vars))
 
-(defn- bindings->tasks [ex binding-pairs]
-  (let [vars        (vars-of binding-pairs)
-        derefs-list (map (fn [v] `(deref ~(symbol (str "fut-" v)))) vars)]
+(defn- bindings->tasks [binding-pairs]
+  (let [vars (vars-of binding-pairs)]
     (->> (map-indexed (fn [i [v sexpr]]
-                        ;; creates a (let [fut-var1 (.submit ... ((partial (fn [args]) (deref arg0))
-                        `[~(symbol (str "fut-" v))
+                        (let [first? (= i 0)
+                              last?  (= i (dec (count binding-pairs)))
+                              fargs  (if first?
+                                       '_
+                                       (into [] (take i vars)))
+                              fbody  (if last? sexpr (into [] (concat (take i vars) [sexpr])))]
 
-                          (.submit ~ex
-                                   ^"Callable"
-                                   (reify Callable
-                                     (~'call [~'this]
-                                       ;; (let [a @fut-a ... n @fut-n] (sexpr-body))
-                                       (let [~@(take (* 2 i) (interleave vars derefs-list))]
-                                          ~sexpr))))])
+                          `[(reify Function
+                              (~'java.util.function.Function/apply [~'this ~fargs] ~fbody))]))
+
                       binding-pairs)
          (apply concat)
-         (into [derefs-list]))))
-
+         (into []))))
 
 (defmacro ^:private sequentially
   "Runs each task sequentially, binding each task to a var, similar to `let`.
@@ -42,16 +40,18 @@
   Each task can depend on a previous value"
   {:forms '[(sequentially [tasks*] exprs*)]}
   [bindings body]
-  (let [binding-pairs (partition 2 bindings)
-        vars          (vars-of binding-pairs)
-        ex            (gensym)
-        [derefs-list & bindings-tasks-pairs] (bindings->tasks ex binding-pairs)]
-    `(let [~(with-meta ex {:tag ExecutorService}) (executor/executor)]
+  (let [ex            (gensym)
+        b             (gensym)
+        binding-pairs (partition 2 (concat bindings [b body]))
+        task-fns      (bindings->tasks binding-pairs)]
+
+    `(let [~(with-meta ex {:tag `ExecutorService}) (executor/executor)]
        (try
-          (let [~@bindings-tasks-pairs]
-            (let [~@(interleave vars derefs-list)]
-              ~body))
-          (finally (.close ~(with-meta ex {:tag ExecutorService})))))))
+         @(.. (CompletableFuture/completedFuture nil)
+              ~@(for [f task-fns]
+                  `(~'thenApplyAsync ~(with-meta f {:tag Function}) ~ex)))
+         (finally
+           (.close ~(with-meta ex {:tag `AutoCloseable})))))))
 
 (clojure.pprint/pprint
   (macroexpand-1
@@ -64,7 +64,6 @@
                b (+ a 1)
                c (+ b a)]
   (+ a b c))
-
 
 (comment
   (set! *warn-on-reflection* true)
@@ -84,36 +83,14 @@
   (cc/bench (sequentially [a 1
                            b (+ a 1)
                            c (+ b a)]
-              (+ a b c)))
-  ;
-  ;Evaluation count : 2092920 in 60 samples of 34882 calls.
-  ;Execution time mean : 30,245814 µs
-  ;Execution time std-deviation : 3,084815 µs
-  ;Execution time lower quantile : 24,993762 µs ( 2,5%)
-  ;Execution time upper quantile : 34,435957 µs (97,5%)
-  ;Overhead used : 4,146448 ns
-
-
-  (cc/quick-bench
-    @(let [^ExecutorService ex (unlearn.virtual.executor/executor)]
-       (.. (CompletableFuture/completedFuture 1)
-           (thenComposeAsync
-             ;; b
-             (reify Function
-               (java.util.function.Function/apply [this a]
-                 (CompletableFuture/completedFuture [a (+ a 1)]))))
-           (thenComposeAsync
-             ;; c
-             (reify Function
-               (java.util.function.Function/apply [this [a b]]
-                 (CompletableFuture/completedFuture [a b (+ a b)]))))
-           (thenComposeAsync
-             ;; res
-             (reify Function
-               (java.util.function.Function/apply [this [a b c]]
-                 (CompletableFuture/completedFuture (+ a b c)))))))))
-
-
+              (+ a b c))))
+;
+;Evaluation count : 2092920 in 60 samples of 34882 calls.
+;Execution time mean : 30,245814 µs
+;Execution time std-deviation : 3,084815 µs
+;Execution time lower quantile : 24,993762 µs ( 2,5%)
+;Execution time upper quantile : 34,435957 µs (97,5%)
+;Overhead used : 4,146448 ns
 
 
 (comment
